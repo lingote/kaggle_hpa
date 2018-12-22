8cd4169b57b5746cc9e794ef53bdf99e9858baf7
aracena <aracena@swissquant.com>
2018-12-22 01:37:58 +0100

    more updates

--- mymodel.py
+++ mymodel.py
@@ -1,28 +1,20 @@
 import tensorflow as tf
 import numpy as np
 import tensornets as tnets
-from dataprep import get_data, get_img_array
+from dataprep import get_datalist, get_img_array
 
 def create_model(input_img, n_out):
     layer_name = 'final_layer_ops'
-    inputs = tf.placeholder(tf.float32, [None, 299,299,3])
-    outputs = tf.placeholder(tf.float32, [None, 28])
-    with tf.name_scope(layer_name):
-        with tf.name_scope('dense_layer'):
-            logits = tf.layers.dense(input_img, n_out,
-                                     kernel_initializer=tf.initializers.he_normal(),
-                                     bias_initializer=tf.initializers.zeros,
-                                     activation='sigmoid', trainable=True)
-        with tf.name_scope('weights'):
-            initial_value = tf.truncated_normal(
-                input_img.shape.as_list(), stddev=0.001
-            )
-            layer_weights = tf.Variable(initial_value, name='final_weights')
+    with tf.variable_scope(layer_name) as scope:
+        inputs = tf.placeholder(tf.float32, [None, 299,299,3])
+        outputs = tf.placeholder(tf.float32, [None, 28])
+        logits = tnets.Inception4(inputs, is_training=True, classes=28)
+        model =tf.identity(logits, name='logits')
+        loss = tf.losses.sigmoid_cross_entropy(outputs, model)
+        train =tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
+        final_tensor = tf.nn.sigmoid(logits, name='final_tensor_name')
 
-        with tf.name_scope('biases'):
-            layer_biases = tf.Variable(tf.zeros([n_out]))
-
-        with tf.name_scope('Wx_plus_b'):
-            logits = tf.matmul(input_img, layer_weights) + layer_biases
-
-    final_tensor = tf.nn.sigmoid(logits, name='final_tensor_name')
+def runmodel(model):
+    with tf.Session() as sess:
+        sess.run(model.pretrained())
+        
